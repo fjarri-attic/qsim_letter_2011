80bd551659a8862e74c6cf1bf5fe0d270388f073
Bogdan Opanchuk <mantihor@gmail.com>
2011-03-23 02:05:51 +1100

    data: added long time Ramsey and spin echo data and simulation scripts

---
+++ data/long_time_ramsey/visibility.py
@@ -0,0 +1,87 @@
+import numpy
+import time
+import math
+
+from beclab import *
+
+def calculateRamsey(wigner, echo):
+
+	t = 8.0
+
+	if wigner:
+		nvx = nvy = 8
+		nvz = 64
+		dt_evo = 4e-5
+	else:
+		nvx = nvy = 16
+		nvz = 128
+		dt_evo = 1e-5
+
+	m = Model(N=44000, detuning=-37,
+		nvx=nvx, nvy=nvy, nvz=nvz, dt_evo=dt_evo, ensembles=256, e_cut=1e6,
+		fx=97.0, fy=97.0 * 1.03, fz=11.69,
+		gamma12=1.52e-20, gamma22=7.7e-20,
+		a12=97.93, a22=95.4)
+
+	constants = Constants(m, double=True)
+	env = envs.cuda(device_num=1)
+	evolution = SplitStepEvolution(env, constants)
+	pulse = Pulse(env, constants)
+
+	a = VisibilityCollector(env, constants, verbose=True)
+	b = PhaseNoiseCollector(env, constants)
+	c = SpinCloudCollector(env, constants)
+
+	collectors = [a, b, c] if wigner else [a]
+
+	gs = GPEGroundState(env, constants)
+
+	cloud = gs.createCloud()
+	if wigner:
+		cloud.toWigner()
+
+	pulse.apply(cloud, 0.5 * math.pi, matrix=True)
+
+	t1 = time.time()
+	if echo:
+		evolution.run(cloud, t / 2, callbacks=collectors, callback_dt=0.002, noise=wigner)
+		pulse.apply(cloud, math.pi, matrix=True)
+		evolution.run(cloud, t / 2, callbacks=collectors, callback_dt=0.002, noise=wigner)
+	else:
+		evolution.run(cloud, t, callbacks=collectors, callback_dt=0.002, noise=wigner)
+	env.synchronize()
+	t2 = time.time()
+	print "Time spent: " + str(t2 - t1) + " s"
+
+	name = ("Spin-echo sequence" if echo else "Ramsey sequence") + ", " + \
+		("Wigner" if wigner else "GPE")
+
+	times, vis = a.getData()
+	vis_data = XYData(name, times, vis, xname="T, s", ymin=0, ymax=1, yname="Visibility")
+
+	if wigner:
+		times, noise = b.getData()
+		noise_data = XYData(name, times, noise, xname="T, s", ymin=0,
+			yname="Phase noise, rad")
+
+		times, phi, yps = c.getData()
+		noise_points = Data('spins', ['phi', 'yps', 'time', 'timename', 'phiname', 'ypsname'],
+			time=time, phi=phi, yps=yps, timename="T, s",
+			phiname="Spin, azimuth, rad", ypsname="Spin, inclination, rad")
+
+	env.release()
+
+	if wigner:
+		return vis_data, noise_data, noise_points
+	else:
+		return vis_data
+
+v1 = calculateRamsey(False, False)
+v1.save('ramsey_gpe_vis.json')
+
+v3, n3, n3_points = calculateRamsey(True, False)
+v3.save('ramsey_wigner_vis.json')
+n3.save('ramsey_wigner_phnoise.json')
+n3_points.save('ramsey_wigner_phnoise_points.pickle')
+
+XYPlot([v1, v3]).save('visibility.pdf')
---
+++ data/long_time_rephasing/rephasing.py
@@ -0,0 +1,109 @@
+import numpy
+import time
+import math
+
+from beclab import *
+from beclab.state import ParticleStatistics, Uncertainty
+
+def runPass(evolution, pulse, cloud, t):
+	pulse.apply(cloud, math.pi, matrix=True)
+	evolution.run(cloud, t, noise=False)
+
+def testRephasing(wigner):
+
+	if wigner:
+		nvx = nvy = 8
+		nvz = 64
+		dt_evo = 4e-5
+	else:
+		nvx = nvy = 16
+		nvz = 128
+		dt_evo = 1e-5
+
+	m = Model(N=44000, detuning=-37,
+		nvx=nvx, nvy=nvy, nvz=nvz, dt_evo=dt_evo, ensembles=64, e_cut=1e6,
+		fx=97.0, fy=97.0 * 1.03, fz=11.69,
+		gamma12=1.52e-20, gamma22=7.7e-20,
+		a12=97.93, a22=95.4)
+
+	t_max = 8.0
+	t_step = 0.05
+
+	constants = Constants(m, double=True)
+	env = envs.cuda()
+	evolution = SplitStepEvolution(env, constants)
+	pulse = Pulse(env, constants)
+
+	stats = ParticleStatistics(env, constants)
+	unc = Uncertainty(env, constants)
+
+	gs = GPEGroundState(env, constants)
+
+	times = [0.0]
+	vis = [1.0]
+	phnoise = []
+	phis = []
+	ypss = []
+
+	cloud = gs.createCloud()
+	if wigner:
+		cloud.toWigner()
+
+		phnoise.append(stats.getPhaseNoise(cloud.a, cloud.b))
+		phi, yps = unc.getSpins(cloud.a, cloud.b)
+		phis.append(phi)
+		ypss.append(yps)
+
+	pulse.apply(cloud, 0.5 * math.pi, matrix=True)
+
+	if wigner:
+		name = 'rephasing_wigner'
+	else:
+		name = 'rephasing_gpe'
+
+	t = t_step
+	while t <= t_max:
+		evolution.run(cloud, t_step / 2, noise=wigner)
+		print "Pre-pi step finished, t=" + str(cloud.time)
+		new_cloud = cloud.copy()
+		pulse.apply(new_cloud, math.pi, matrix=True)
+		evolution.run(new_cloud, t / 2, noise=wigner)
+		print "Post-pi step finished"
+
+		times.append(t)
+		vis.append(stats.getVisibility(new_cloud.a, new_cloud.b))
+		print "Visibility=" + str(vis[-1])
+
+		if wigner:
+			phnoise.append(stats.getPhaseNoise(new_cloud.a, new_cloud.b))
+			phi, yps = unc.getSpins(new_cloud.a, new_cloud.b)
+			phis.append(phi)
+			ypss.append(yps)
+
+		XYData(name, numpy.array(times), numpy.array(vis),
+			ymin=0, ymax=1, xname="T, s", yname="Visibility").save(name + '_vis.json')
+
+		if wigner:
+			XYData(name, numpy.array(times), numpy.array(phnoise), ymin=0,
+				xname="T, s", yname="Phase noise, rad").save(name + '_phnoise.json')
+
+			Data('spins',
+				['name', 'phi', 'yps', 'time', 'timename', 'phiname', 'ypsname'],
+				time=numpy.array(times), phi=numpy.array(phis), yps=numpy.array(ypss), timename="T, s",
+				phiname="Spin, azimuth, rad", ypsname="Spin, inclination, rad",
+				name=name).save(name + "_phnoise_points.pickle")
+
+		XYPlot([XYData.load(name + '_vis.json')]).save(name + '_vis.pdf')
+
+		del new_cloud
+		t += t_step
+
+	env.release()
+
+
+testRephasing(False)
+testRephasing(True)
+
+vis_gpe = XYData.load('rephasing_gpe_vis.json')
+vis = XYData.load('rephasing_wigner_vis.json')
+XYPlot([vis_gpe, vis]).save('visibility.pdf')
